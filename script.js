8ce41da1ab8615038be5906183769d23c0519ee7
Yousef <160142268+JooScript@users.noreply.github.com>
2025-11-21 17:44:59 +0200

    Make Tools Section

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+ fetch('./files/tools.json')
+    .then(res => res.json())
+    .then(data => {
+      const container = document.getElementById('tools-container');
+      data.tools.forEach(tool => {
+        container.innerHTML += `
+      <span class="tool-tag">
+        <img src="./imgs/checkmark.png" alt="${tool.name}" title="${tool.name}"/>
+        ${tool.name}
+      </span>
+        `;
+      });
+    });
